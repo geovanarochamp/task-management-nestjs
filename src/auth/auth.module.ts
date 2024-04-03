bea4eb801c1a9f548815a4048f0eba94aebb1ea5
geovanarochamp <geovana.rocha.martins@gmail.com>
2024-04-03 11:03:30 -0300

    task-management-nestjs

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Jwtstrategy } from './jwt.strategy';
+import { UsersRepository } from './users.repository';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
+  providers: [AuthService, Jwtstrategy],
+  controllers: [AuthController],
+  exports: [Jwtstrategy, PassportModule],
+})
+export class AuthModule {}
